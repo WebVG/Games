727312d9fdcc9abc730858ca4ec7e3cb4eb8f355
Garrett <GVWebster.91@gmail.com>
2017-09-02 13:44:18 -0400

    Add files via upload

--- TextAdventure.lisp
+++ TextAdventure.lisp
@@ -80,6 +80,51 @@
 
 (defun inventory ()
 	(cons 'items- (objects-at 'body *objects* *object-locations*)))
+#|start creation of game specific read-eval-print-loop
+	below are also custom read, eval and print functions to use
+|#
+
+(defun game-repl()
+	(let ((cmd (game-read)))
+		(unless (eq (car cmd) 'quit)
+			(game-print (game-eval cmd))
+			(game-repl))))
+
+(defun game-read ()
+	(let ((cmd (read-from-string
+					(concatenate 'string "(" (read-line) ")"))))
+					(flet ((quote-it (x)
+									(list 'quote x)))
+						(cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
+
+(defparameter *allowed-commands* '(look walk pickup inventory))
+
+(defun game-eval (sexp)
+	(if (member (car sexp) *allowed-commands*)
+		(eval sexp)
+		'(i do not know that command.)))
+
+(defun tweak-text (lst caps lit)
+ (when lst
+ (let ((item (car lst))
+		(rest (cdr lst)))
+		(cond ((eq item #\space) (cons item (tweak-text rest caps lit)))
+			  ((member item '(#\! #\? #\.)) (cons item (tweak-text rest t lit)))
+			  ((eq item #\") (tweak-text rest caps (not lit)))
+			   (lit (cons item (tweak-text rest nil lit)))
+			  ((or caps lit) (cons (char-upcase item) (tweak-text rest nil lit)))
+			  (t (cons (char-downcase item) (tweak-text rest nil nil)))))))
+
+(defun game-print (lst)
+	(princ (coerce (tweak-text (coerce (string-trim "() "
+													 (prin1-to-string lst))
+										'list)
+								t
+								nil)
+					'string))
+	(fresh-line))
+(game-print '(not only does this sentence have a "comma," it also mentions the "iPad."))
+
 #|
 controls
 (look)
